84bb011a5210dcdb2155ac9cc6abce7c09a5d08b
GerganaNoneva <gergananoneva@gmail.com>
2023-01-16 16:55:17 +0200

    Add bit

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,16 +10,23 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+    this._beat =new Beat();
+    this._create();
+    this.emit(Application.events.READY);
+  }
 
+  _create() {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+    this._beat.on(Beat.events.BIT, (bit,index) => {
+      const message = document.createElement("div");
+      message.classList.add("message");
+      console.log(bit)
+      message.innerText = lyrics[bit];
 
-    document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
+      document.querySelector(".main").appendChild(message);
+    })
   }
 }
+
