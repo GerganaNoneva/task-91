f40dcb97f275b95040be5200dd8c4e31410dc0a1
GerganaNoneva <gergananoneva@gmail.com>
2023-01-16 17:28:24 +0200

    hm

--- src/js/Beat.js
+++ src/js/Beat.js
@@ -15,21 +15,3 @@ export default class Beat extends EventEmitter {
     }, 600);
   }
 }
-
-/*
-import EventEmitter from "eventemitter3";
-export default class Beat extends EventEmitter{
-  static get events() {
-    return {
-      BIT: "bit",
-    };
-  }
-
-  constructor() {
-    super();
-    setInterval(() => {
-      console.log("bit");
-      this.emit(Beat.events.BIT);
-    }, 600);
-  }
-}*/
