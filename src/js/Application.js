3a4967a2a9eed35cb490959d6403cba19e5ab7af
GerganaNoneva <gergananoneva@gmail.com>
2023-01-16 16:38:34 +0200

    Corect

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+
 import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
@@ -14,7 +15,7 @@ export default class Application extends EventEmitter {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    this._beat.on(Beat.events.BIT, (bit) => {
+      this._beat.on(Beat.events.BIT, (bit) => {
       const message = document.createElement("div");
       message.classList.add("message");
       message.innerText = lyrics[bit];
